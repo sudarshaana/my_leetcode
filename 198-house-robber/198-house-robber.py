5f12fc0a53fdda4eacd2bb7ef28d806b5eb5223b
Sudarshan Mondal <sudarshaana@gmail.com>
2022-05-16 16:36:52 +0600

    Time: 49 ms (34.58%), Space: 13.8 MB (67.56%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,20 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-    
-        if len(nums) == 1:
-            return nums[0]
-    
-        # bottomup table init
-        dp = [0]*(len(nums))
+        
+        num0, num1 = 0,0
 
-        # initial base case
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
-            #print(i)
-
-        # returning last index of our table
-        return dp[-1]
+        for n in nums:
+            temp = max(num1, num0+n)
+            num0 = num1
+            num1 = temp
+        return num1
