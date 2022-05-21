ac0626d4ebef357d9bd195a60aceef999a3b0c71
Sudarshan Mondal <sudarshaana@gmail.com>
2022-05-21 17:19:20 +0600

    Time: 53 ms (34.22%), Space: 13.9 MB (24.72%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,4 +1,30 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
-        return '{0:b}'.format(int(a, 2) + int(b, 2))
+        max_len = max(len(a), len(b))
+        carry = 0
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
+
+        ans = []
+        for i in range(max_len-1, -1, -1):
+            if a[i] == '1':
+                carry+=1
+            if b[i] == '1':
+                carry+=1
+
+            '''
+            if both a, b and c  are 1, c is 3, we'll add 1
+            if one of the 3 is 0, c will be 2 % 2 == 0 
+            '''
+            if carry%2 == 1: # c ==3
+                ans.append("1")
+            else:
+                ans.append("0")
+            carry //= 2
+
+        if carry == 1:
+            ans.append("1")
+
+        ans.reverse()
+        return "".join(ans)
