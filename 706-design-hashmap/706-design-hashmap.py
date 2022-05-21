eb5d01c1090e4eb8dd7c37b03b83cff051fe778f
Sudarshan Mondal <sudarshaana@gmail.com>
2022-05-22 00:28:46 +0600

    Time: 447 ms (33.99%), Space: 18.2 MB (30.73%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,28 +1,52 @@
+class Bucket:
+    def __init__(self):
+        self.bucket = []
+        
+    def update(self, key, val):
+        found = False
+        for i, kv in enumerate(self.bucket):
+            if kv[0] == key:
+                self.bucket[i] = (key, val)
+                found = True
+            if found:
+                break
+        if not found:
+            self.bucket.append((key, val))
+            
+    def get(self, key):
+        for k,v in self.bucket:
+            if k == key:
+                return v
+        return -1
+                
+    
+    def remove(self, key):
+        for i, kv in enumerate(self.bucket):
+            if kv[0] == key:
+                del self.bucket[i]
+                
+        
+
 class MyHashMap:
     
     def __init__(self):
-        self.data = []
+        self.key_space = 2069
+        self.hash_table = [Bucket() for i in range(self.key_space)]
         
 
     def put(self, key: int, value: int) -> None:
-        for i, d in enumerate(self.data):
-            if d[0] == key:
-                self.data[i] = [key, value]
-                return
-        
-        self.data.append([key, value])
-        
+        hash_key = key % self.key_space
+        self.hash_table[hash_key].update(key, value)
 
     def get(self, key: int) -> int:
-        for d in self.data:
-            if d[0] == key:
-                return d[1]
-        return -1
+        hash_key = key % self.key_space 
+        return self.hash_table[hash_key].get(key)
 
-    def remove(self, key: int) -> None:
-        for d in self.data:
-            if d[0] == key:
-                self.data.remove(d)
+
+    def remove(self, key: int) -> None:        
+        hash_key = key % self.key_space 
+        self.hash_table[hash_key].remove(key)
+                
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
