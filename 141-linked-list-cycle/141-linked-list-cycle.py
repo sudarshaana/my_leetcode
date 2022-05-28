e469af021aebe303342644859fd31c4fcee0d1c4
Sudarshan Mondal <sudarshaana@gmail.com>
2022-05-29 01:22:58 +0600

    Time: 93 ms (26.19%), Space: 17.6 MB (66.36%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,12 +6,25 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hasmap = set()
-        node = head
-        while node is not None:
-            if node not in hasmap:
-                hasmap.add(node)
-            else:
+        # using hasmap
+        # hasmap = set()
+        # node = head
+        # while node is not None:
+        #     if node not in hasmap:
+        #         hasmap.add(node)
+        #     else:
+        #         return True
+        #     node = node.next
+        # return False
+        
+        # using two pointer
+        
+        fast = head
+        slow = head
+        while fast and fast.next:
+            slow = slow.next 
+            fast = fast.next.next
+            
+            if fast == slow:
                 return True
-            node = node.next
         return False
