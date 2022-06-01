11c532788d50ffa237ecf92be8003c4f9f418584
Sudarshan Mondal <sudarshaana@gmail.com>
2022-06-01 13:47:41 +0600

    Time: 66 ms (53.91%), Space: 17.4 MB (57.00%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -4,15 +4,37 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # using hasmap
-        hasmap = set()
-        node = head
-        while node is not None:
-            if node not in hasmap:
-                hasmap.add(node)
-            else:
-                return node
-            node = node.next
+    
+    def intersection(self, head):    
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next 
+
+            if fast == slow:
+                return slow
         return None
+        
+        
+    def detectCycle(self, head) -> bool:
+        
+        if not head:
+            return None
+    
+        cycle = self.intersection(head)
+        if cycle is None:
+            return None
+        
+        node1 = head
+        node2 = cycle
+        
+        while node1 != node2:
+            node1 = node1.next
+            node2 = node2.next
+            
+        
+        return node1
