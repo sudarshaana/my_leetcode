463cd0ca30b2061ee31244aafbee8d55aec14d22
Sudarshan Mondal <sudarshaana@gmail.com>
2022-06-01 15:57:56 +0600

    Time: 290 ms, Memory: 30 MB - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -6,25 +6,37 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        hashset = set()
-        nodeA = headA
-        nodeB = headB
+#         hashset = set()
+#         nodeA = headA
+#         nodeB = headB
         
-        while nodeA or nodeB:
+#         while nodeA or nodeB:
         
-            if nodeA is not None:
-                if nodeA in hashset:
-                    return nodeA
-                hashset.add(nodeA)
+#             if nodeA is not None:
+#                 if nodeA in hashset:
+#                     return nodeA
+#                 hashset.add(nodeA)
             
-            if nodeB is not None:
-                if nodeB in hashset:
-                    return nodeB
-                hashset.add(nodeB)
+#             if nodeB is not None:
+#                 if nodeB in hashset:
+#                     return nodeB
+#                 hashset.add(nodeB)
             
-            if nodeA is not None:
-                nodeA = nodeA.next
-            if nodeB is not None:
-                nodeB = nodeB.next
+#             if nodeA is not None:
+#                 nodeA = nodeA.next
+#             if nodeB is not None:
+#                 nodeB = nodeB.next
                         
+#         return None
+        hashset =set()
+        node = headA
+        while node:
+            hashset.add(node)
+            node = node.next
+            
+        node = headB
+        while node:
+            if node in hashset:
+                return node
+            node = node.next
         return None
