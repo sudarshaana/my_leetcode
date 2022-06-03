de47569c60e26f56a4ceebb455544767bb2f90b0
Sudarshan Mondal <sudarshaana@gmail.com>
2022-06-04 00:13:49 +0600

    Time: 40 ms (69.23%), Space: 13.9 MB (19.63%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,26 +5,44 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        size = 0
-        node = head
-        while node:
-            size+=1
-            node = node.next
+#         size = 0
+#         node = head
+#         while node:
+#             size+=1
+#             node = node.next
         
-        node = head
-        target_node_position = size - n 
+#         node = head
+#         target_node_position = size - n 
         
-        # head
-        if target_node_position == 0:
+#         # head
+#         if target_node_position == 0:
+#             return head.next
+        
+#         target_node_position -=1
+       
+#         # head is removed, now the rest of them
+#         for i in range(target_node_position+1):
+#             if i == target_node_position:
+#                 node.next = node.next.next
+#             node = node.next
+                
+#         return head 
+                
+         # two pointer approach
+        current_node = head
+        for i in range(n):
+            current_node=current_node.next
+        
+        if current_node is None:
             return head.next
         
-        target_node_position -=1
-       
-        # head is removed, now the rest of them
-        for i in range(target_node_position+1):
-            if i == target_node_position:
-                node.next = node.next.next
-            node = node.next
-                
-        return head 
-                
+        node_before_remove = head
+        
+        while current_node.next is not None:
+            current_node = current_node.next
+            node_before_remove = node_before_remove.next
+            
+            
+        node_before_remove.next = node_before_remove.next.next
+            
+        return head
