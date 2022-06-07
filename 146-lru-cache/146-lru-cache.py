7e269fd34d81b2f6e5f3ccde6964e55c34e9d8ab
Sudarshan Mondal <sudarshaana@gmail.com>
2022-06-07 16:00:44 +0600

    Time: 1101 ms (51.22%), Space: 74.6 MB (92.63%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,18 +1,58 @@
 class LRUCache:
 
+#     def __init__(self, capacity: int):
+#         self.capacity= capacity
+#         self.size = 0
+#         self.access_list = []
+#         self.cache = {}
+        
+
+#     def get(self, key: int) -> int:
+        
+#         if key in self.cache:
+#             self.access_list.remove(key)
+#             self.access_list.append(key)
+#             return self.cache[key]
+#         else:
+#             return -1
+        
+
+#     def put(self, key: int, value: int) -> None:
+        
+#         if key in self.cache:
+#             self.cache[key] = value
+            
+#             self.access_list.remove(key)
+#             self.access_list.append(key)
+            
+#         else:
+#             self.cache[key] = value
+#             #print("adding {}:{}".format(key, value))
+#             # add to cache
+#             self.size+=1
+        
+#             # removing lru if full
+#             self.access_list.append(key)
+#             if len(self.access_list) > self.capacity:
+#                 hash_key = self.access_list.pop(0)
+#                 del self.cache[hash_key]
+#                 self.size -= 1
+
+# From python 3.7 dict guarantees that order will be kept, and popitem will pop LIFO order but we need LIFO type system
+# so we need OrderedDict which have popIten(last = T/F) which satisfy out req
+
     def __init__(self, capacity: int):
         self.capacity= capacity
         self.size = 0
-        self.access_list = []
-        self.cache = {}
+        self.cache = OrderedDict()
         
 
     def get(self, key: int) -> int:
         
         if key in self.cache:
-            self.access_list.remove(key)
-            self.access_list.append(key)
-            return self.cache[key]
+            val = self.cache.pop(key)
+            self.cache[key] = val
+            return val
         else:
             return -1
         
@@ -20,23 +60,19 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         
         if key in self.cache:
+            self.cache.pop(key)
             self.cache[key] = value
             
-            self.access_list.remove(key)
-            self.access_list.append(key)
-            
-        else:
+        else:            
+            # check size
+            if self.size == self.capacity:
+                # remove LRUCache
+                val = self.cache.popitem(last = False)
+                self.size-=1
             self.cache[key] = value
-            #print("adding {}:{}".format(key, value))
-            # add to cache
+            
             self.size+=1
-        
-            # removing lru if full
-            self.access_list.append(key)
-            if len(self.access_list) > self.capacity:
-                hash_key = self.access_list.pop(0)
-                del self.cache[hash_key]
-                self.size -= 1
+            
         
 
 # Your LRUCache object will be instantiated and called as such:
