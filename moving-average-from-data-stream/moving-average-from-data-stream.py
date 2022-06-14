5e6cc1fadd689c7be5dfd56c0bb1c116ac2a33ab
Sudarshan Mondal <sudarshaana@gmail.com>
2022-06-14 23:18:53 +0600

    Time: 170 ms, Memory: 17.1 MB - LeetHub

--- moving-average-from-data-stream/moving-average-from-data-stream.py
+++ moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -1,13 +1,25 @@
+from collections import deque
+
 class MovingAverage:
 
-    def __init__(self, size: int):
-        self.number = size
-        self.queue = []
+#     def __init__(self, size: int):
+#         self.number = size
+#         self.queue = []
 
-    def next(self, val: int) -> float:
+#     def next(self, val: int) -> float:
+#         if len(self.queue) == self.number:
+#             self.queue.pop(0)
+#         self.queue.append(val)
+#         return sum(self.queue)/len(self.queue)
+    def __init__(self, number):
+        self.number = number
+        self.queue = deque()
+        
+    
+    def next(self, number):
         if len(self.queue) == self.number:
-            self.queue.pop(0)
-        self.queue.append(val)
+            self.queue.popleft()
+        self.queue.append(number)
         return sum(self.queue)/len(self.queue)
 
 
