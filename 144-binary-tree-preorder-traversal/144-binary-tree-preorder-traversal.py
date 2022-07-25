9c1ff95365715ccc7dfd4e22c3a32c7bfae96b54
Sudarshan Mondal <sudarshaana@gmail.com>
2022-07-26 00:25:17 +0600

    Time: 48 ms (47.59%), Space: 13.8 MB (96.54%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -9,10 +9,24 @@ class Solution:
         self.result = []
         
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        """ Pre-order Tree traversal with recursive function call. root -> left -> Right """
-        if root:
-            self.result.append(root.val)
-            self.preorderTraversal(root.left)
-            self.preorderTraversal(root.right)
+#         """ Pre-order Tree traversal with recursive function call. root -> left -> Right """
+#         if root:
+#             self.result.append(root.val)
+#             self.preorderTraversal(root.left)
+#             self.preorderTraversal(root.right)
         
+#         return self.result
+
+        """ Pre-order traversal with iterative manner"""
+        stack = []
+        current = root
+        while current or stack:
+            while current:
+                self.result.append(current.val)
+                stack.append(current)
+                current = current.left
+            
+            current = stack.pop()
+            current = current.right
+            
         return self.result
