cdfcc7bb764b4401bac02a04a663952a56d63e9b
Sudarshan Mondal <sudarshaana@gmail.com>
2022-07-27 23:35:19 +0600

    Time: 49 ms (42.22%), Space: 14 MB (24.92%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -1,12 +1,10 @@
 class MyStack:
-
     """
-    Using Two Queue
+    Using One Queue
     """
     
     def __init__(self):
         self.q1 = []
-        self.q2 = []
         self.top_val = None
 
     def push(self, x: int) -> None:
@@ -15,24 +13,55 @@ class MyStack:
         
 
     def pop(self) -> int:
-        
-        while len(self.q1) > 1:
-            # pop element and add to q2
+        size  = len(self.q1)
+        while size > 1:
             item = self.q1.pop(0)
+            self.q1.append(item)
             self.top_val = item
-            self.q2.append(item)
-        # performing stack pop
-        val = self.q1.pop()
-        # copying q2 to q1
-        self.q1 = self.q2.copy()
-        self.q2 = []
-        return val
+            size = size - 1
+        
+        return self.q1.pop(0)
             
     def top(self) -> int:
         return self.top_val
 
     def empty(self) -> bool:
-        return len(self.q1) == 0
+        return len(self.q1)==0
+    
+
+#     """
+#     Using Two Queue
+#     """
+    
+#     def __init__(self):
+#         self.q1 = []
+#         self.q2 = []
+#         self.top_val = None
+
+#     def push(self, x: int) -> None:
+#         self.q1.append(x)
+#         self.top_val = x
+        
+
+#     def pop(self) -> int:
+        
+#         while len(self.q1) > 1:
+#             # pop element and add to q2
+#             item = self.q1.pop(0)
+#             self.top_val = item
+#             self.q2.append(item)
+#         # performing stack pop
+#         val = self.q1.pop()
+#         # copying q2 to q1
+#         self.q1 = self.q2.copy()
+#         self.q2 = []
+#         return val
+            
+#     def top(self) -> int:
+#         return self.top_val
+
+#     def empty(self) -> bool:
+#         return len(self.q1) == 0
     
     
 # Your MyStack object will be instantiated and called as such:
