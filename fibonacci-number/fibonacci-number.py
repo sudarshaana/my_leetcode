981be037ef699f2bc57971b4f6bf005cb4df4e18
Sudarshan Mondal <sudarshaana@gmail.com>
2022-07-29 01:45:59 +0600

    Time: 60 ms, Memory: 14 MB - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,9 +1,15 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    def __init__(self):
+        self.visited = {}
         
-        if n == 0:
+    def fib(self, n, visited=None):
+        if n in self.visited:
+            return self.visited[n] 
+        
+        if n <= 0:
             return 0
         if n == 1:
             return 1
         
-        return self.fib(n-1)+self.fib(n-2)
+        self.visited[n] =  self.fib(n-1, self.visited)+self.fib(n-2, self.visited)
+        return self.visited[n]
