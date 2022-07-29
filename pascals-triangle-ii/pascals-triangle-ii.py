c2497f0a3345fea8c71f2e8ebb7b9319109ddb25
Sudarshan Mondal <sudarshaana@gmail.com>
2022-07-29 12:49:37 +0600

    Time: 56 ms, Memory: 14.1 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.py
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -2,18 +2,27 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         
         
-        def value(i, j, memory=None):
-            if (i,j) in memory:
-                return memory[(i,j)]
+        # def value(i, j, memory=None):
+        #     if (i,j) in memory:
+        #         return memory[(i,j)]
+        #     if i == j or j == 0:
+        #         return 1
+        #     memory[(i,j)] = value(i-1, j-1, memory) + value(i-1, j, memory)
+        #     return memory[(i,j)]
+        # memory = {}
+        # l = []
+        # for i in range(rowIndex+1):
+        #     l.append(value(rowIndex, i, memory))
+        # return l
+        
+        from functools import lru_cache
+        @lru_cache()
+        def value(i, j):
             if i == j or j == 0:
                 return 1
-            
-            
-            memory[(i,j)] = value(i-1, j-1, memory) + value(i-1, j, memory)
-            return memory[(i,j)]
+            return value(i-1, j-1) + value(i-1, j)
         
-        memory = {}
         l = []
         for i in range(rowIndex+1):
-            l.append(value(rowIndex, i, memory))
+            l.append(value(rowIndex, i))
         return l
