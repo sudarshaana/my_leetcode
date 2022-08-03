0b9b9d6eb99767dc730e829b380af284d88f86ee
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-03 22:33:07 +0600

    Time: 50 ms (61.31%), Space: 14 MB (14.36%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -3,10 +3,17 @@ class Solution:
         hashset = set()
         
         while n != 1:
-            s = list(map(int, str(n)))
-            n = 0
-            for num in s:
-                n += num*num
+#             s = list(map(int, str(n)))
+#             n = 0
+#             for num in s:
+#                 n += num*num
+             
+            total = 0
+            while n//10 != 0:
+                total += (n%10)**2
+                n = n// 10
+            n = n*n + total
+
             if n in hashset:
                 return False
             else:
