85a31479fcadd9af242568ac39b6974bb585ab6d
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-09 23:58:26 +0600

    Time: 233 ms, Memory: 14.3 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -2,18 +2,27 @@ from collections import OrderedDict
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         
-        #first_occurance = -1
         hash_map = OrderedDict()
-        
+        index = {}
         
         for i, char in enumerate(s):
-            #if not char in hash_map:
-            #    hash_map[char]=1
-            #else:
             hash_map[char]=hash_map.get(char, 0) + 1
+            index[char]=i
             
         for i, k in enumerate(hash_map.keys()):
             if hash_map[k]==1:
-                return s.find(k)
+                return index[k]
+        
         return -1
+        
+#         hash_map = OrderedDict()
+        
+#         for i, char in enumerate(s):
+#             hash_map[char]=hash_map.get(char, 0) + 1
+            
+#         for i, k in enumerate(hash_map.keys()):
+#             if hash_map[k]==1:
+#                 return s.find(k)
+#         return -1
+
     
