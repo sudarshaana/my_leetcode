258b994841b6d250d6fccbed3b6e50c1c14206bf
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-16 00:05:30 +0600

    Time: 1174 ms (62.35%), Space: 18.1 MB (72.41%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -11,20 +11,36 @@ class Solution:
         return ans
     
     def twoSum(self, i, nums, res):
-        target = -nums[i]
-        l = i+1
-        r = len(nums)-1
+        seen = set()
+        j = i+1
+        r = len(nums)
         
-        while l<r:
-            sum = nums[l]+nums[r]
+        while j < r:
+            com = -nums[i] - nums[j]
             
-            if sum > target:
-                r-=1
-            elif sum < target:
-                l+=1
-            else:
-                res.append( [-target, nums[l], nums[r] ])
-                l+=1
-                r-=1
-                while l<r and nums[l]==nums[l-1]:
-                    l+=1
+            if com in seen:
+                res.append([nums[i], nums[j], com])
+                
+                while j + 1 < r and nums[j] == nums[j+1]:
+                    j+=1
+            seen.add(nums[j])
+            j+=1
+            
+        
+#         target = -nums[i]
+#         l = i+1
+#         r = len(nums)-1
+        
+#         while l<r:
+#             sum = nums[l]+nums[r]
+            
+#             if sum > target:
+#                 r-=1
+#             elif sum < target:
+#                 l+=1
+#             else:
+#                 res.append( [-target, nums[l], nums[r] ])
+#                 l+=1
+#                 r-=1
+#                 while l<r and nums[l]==nums[l-1]:
+#                     l+=1
