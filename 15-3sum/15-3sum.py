11b78abec760fbeb51073188435991058c7e0e6c
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-16 23:40:08 +0600

    Time: 8556 ms (5.00%), Space: 18.4 MB (16.12%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,30 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        nums.sort()
         
-        for i in range(len(nums)):
-            if  nums[i]>0:
-                break
-            if i == 0 or nums[i-1] != nums[i]:
-                self.twoSum(i, nums, ans)
-        return ans
+        """ Using dic, no short """
+        res, dups = set(), set()
+        seen = {}
+        
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 -val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res
+        
+        
+#         ans = []
+#         nums.sort()
+        
+#         for i in range(len(nums)):
+#             if  nums[i]>0:
+#                 break
+#             if i == 0 or nums[i-1] != nums[i]:
+#                 self.twoSum(i, nums, ans)
+#         return ans
     
-    def twoSum(self, i, nums, res):
-        seen = set()
-        j = i+1
-        r = len(nums)
+#     def twoSum(self, i, nums, res):
+#         seen = set()
+#         j = i+1
+#         r = len(nums)
         
-        while j < r:
-            com = -nums[i] - nums[j]
+#         while j < r:
+#             com = -nums[i] - nums[j]
             
-            if com in seen:
-                res.append([nums[i], nums[j], com])
+#             if com in seen:
+#                 res.append([nums[i], nums[j], com])
                 
-                while j + 1 < r and nums[j] == nums[j+1]:
-                    j+=1
-            seen.add(nums[j])
-            j+=1
+#                 while j + 1 < r and nums[j] == nums[j+1]:
+#                     j+=1
+#             seen.add(nums[j])
+#             j+=1
             
         
 #         target = -nums[i]
