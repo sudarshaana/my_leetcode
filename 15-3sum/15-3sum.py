36da62c612422d1773bbce54dbaa55f8164bffb7
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-16 23:43:17 +0600

    Time: 1507 ms (42.70%), Space: 18.6 MB (13.23%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -7,6 +7,7 @@ class Solution:
         
         for i, val1 in enumerate(nums):
             if val1 not in dups:
+                dups.add(val1)
                 for j, val2 in enumerate(nums[i+1:]):
                     complement = -val1 -val2
                     if complement in seen and seen[complement] == i:
