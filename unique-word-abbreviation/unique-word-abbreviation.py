0c29a0a42534222534827c82bee81668bf9f48ee
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-22 21:20:39 +0600

    Time: 239 ms, Memory: 24.5 MB - LeetHub

--- unique-word-abbreviation/unique-word-abbreviation.py
+++ unique-word-abbreviation/unique-word-abbreviation.py
@@ -2,40 +2,22 @@ class ValidWordAbbr:
 
     def __init__(self, dictionary: List[str]):
         self.d = {}
-        for w in dictionary:
-            if len(w)>2:
-                abr = w[0] + str(len(w[1:-1])) + w[-1]
-                
-                saved_w = self.d.get(abr, [])
-                if saved_w!=[] and saved_w == [w]:
-                    continue
-                self.d[abr] = self.d.get(abr, []) + [w]
-                
-            else:
-                saved_w = self.d.get(w, [])
-                if saved_w != [] and saved_w == [w]:
-                    continue
-                self.d[w] = self.d.get(w, []) + [w]
-
-    def isUnique(self, word: str) -> bool:
         
-        if len(word)>2:
-            abr = word[0] + str(len(word[1:-1])) + word[-1]
-            
-        else:
-            abr = word
-            
+        for w in dictionary:
+            abr = w if len(w) <= 2 else w[0] + str(len(w[1:-1])) + w[-1]    
+            if abr in self.d and self.d[abr] != w:
+                self.d[abr] = "-"
+            else:
+                self.d[abr] = w
+
+    def isUnique(self, w: str) -> bool:
+        abr = w if len(w) <= 2 else w[0] + str(len(w[1:-1])) + w[-1]  
+        
         if abr not in self.d:
             return True
-        
-        else:
-            if len(self.d[abr])>1:
-                return False
-            
-            elif self.d[abr][0] == word:
-                return True
-            
-            return False
+        elif self.d[abr] == w:
+            return True
+        return False
             
 
 # Your ValidWordAbbr object will be instantiated and called as such:
