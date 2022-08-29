172e2d9fdc4e8a4755783f80da327900efcdc7e1
Sudarshan Mondal <sudarshaana@gmail.com>
2022-08-29 09:27:51 +0600

    Time: 104 ms (100.00%), Space: 14.1 MB (80.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -2,29 +2,32 @@ class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         
         nums.sort()
-    
-        ans = []
         
-        for q in queries:
+        accumulated_data = list(accumulate(nums))
+        return [bisect_right(accumulated_data, q) for q in queries]
+    
+#         ans = []
+        
+#         for q in queries:
             
-            way = -1
-            s = 0
-            for i, num in enumerate(nums):
-                s+=num
+#             way = -1
+#             s = 0
+#             for i, num in enumerate(nums):
+#                 s+=num
                 
-                if num > q:
-                    way = i
-                    break
+#                 if num > q:
+#                     way = i
+#                     break
                 
-                elif s > q:
-                    way = i
-                    break
+#                 elif s > q:
+#                     way = i
+#                     break
                     
-                elif s == q:
-                    way = i+1
-                    break
-            if way == -1:
-                way = len(nums)
-            ans.append(way)
+#                 elif s == q:
+#                     way = i+1
+#                     break
+#             if way == -1:
+#                 way = len(nums)
+#             ans.append(way)
                     
-        return ans
+#         return ans
