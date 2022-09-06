ad356b6439dc9c365c53fe69cdf31c7192a56e45
Sudarshan Mondal <sudarshaana@gmail.com>
2022-09-06 21:00:54 +0600

    Time: 118 ms (90.40%), Space: 14.3 MB (51.53%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,17 +1,17 @@
 class Solution:
     
-    def binary_search(self, data):
-        low = 0
-        high = len(data)
+#     def binary_search(self, data):
+#         low = 0
+#         high = len(data)
         
-        while low < high:
-            mid = low + (high-low)//2
-            if data[mid] == 1:
-                low = mid + 1
-            else:
-                high = mid
+#         while low < high:
+#             mid = low + (high-low)//2
+#             if data[mid] == 1:
+#                 low = mid + 1
+#             else:
+#                 high = mid
             
-        return low
+#         return low
     
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         
@@ -21,19 +21,38 @@ class Solution:
         # smallest = heapq.nsmallest(k, heap)
         # return [num[1] for num in smallest]
         
-        heapq.heapify(heap)
+#         heapq.heapify(heap)
         
-        for i, row in enumerate(mat):
+#         for i, row in enumerate(mat):
             
-            heapq.heappush(heap, (-self.binary_search(row), -i))
+#             heapq.heappush(heap, (-self.binary_search(row), -i))
             
-            if len(heap) > k:
-                heapq.heappop(heap)
+#             if len(heap) > k:
+#                 heapq.heappop(heap)
                 
-        #print(heap)
-        res = []
-        while heap:
-            res.append(-heapq.heappop(heap)[1])
+#         #print(heap)
+#         res = []
+#         while heap:
+#             res.append(-heapq.heappop(heap)[1])
             
-        return res[::-1]
-            
+#         return res[::-1]
+        
+        m, n = len(mat), len(mat[0])
+        indexes = []
+        
+        for c, r in product(range(n), range(m)):
+            if len(indexes) == k:
+                break
+            if mat[r][c] == 0 and (c == 0 or mat[r][c-1] == 1):
+                indexes.append(r)
+                
+                
+        i = 0
+        while len(indexes) < k:
+
+            if mat[i][-1] == 1:
+                indexes.append(i)
+            i+=1
+                    
+        return indexes
+        
