51f602fc09e6d36dbd8e1cda7ddaee72e0c5f12c
Sudarshan Mondal <sudarshaana@gmail.com>
2022-09-17 23:38:48 +0600

    Time: 394 ms (12.43%), Space: 18.4 MB (20.45%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -3,23 +3,52 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+from queue import PriorityQueue
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        heap = []
-        heapq.heapify(heap)
+        class Wrapper():
+            def __init__(self, node):
+                self.node = node
+            def __lt__(self, other):
+                return self.node.val < other.node.val
+            
+        
+        q = PriorityQueue()
         
         for l in lists:
-            
-            head = l
-            while head:
-                heapq.heappush(heap, head.val)
-                head = head.next
-        
+            if l:
+                q.put(Wrapper(l))
+                
         senti = ListNode(0)
         head = senti
-        while heap:
-            head.next = ListNode(heapq.heappop(heap))
+        while not q.empty():
+            node = q.get().node
+            head.next = node
             head = head.next
+            if node.next:
+                q.put(Wrapper(node.next))
         
         return senti.next
+        
+        
+        
+#         heap = []
+#         heapq.heapify(heap)
+        
+#         for l in lists:
+            
+#             head = l
+#             while head:
+#                 heapq.heappush(heap, head.val)
+#                 head = head.next
+        
+#         senti = ListNode(0)
+#         head = senti
+#         while heap:
+#             head.next = ListNode(heapq.heappop(heap))
+#             head = head.next
+        
+#         return senti.next
