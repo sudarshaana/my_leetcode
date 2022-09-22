a010cf6765015e450094d6767a3a78426b932793
Sudarshan Mondal <sudarshaana@gmail.com>
2022-09-23 00:33:23 +0600

    Time: 38 ms, Memory: 13.8 MB - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -3,11 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+#         for i in range(len(nums)):
+#             min_index = i
+            
+#             for j in range(i+1, len(nums)):
+#                 if nums[min_index] > nums[j]:
+#                     min_index = j
+            
+#             nums[min_index], nums[i] =   nums[i] , nums[min_index]
+
+
+        # using counting sort
+        M = 2
+        counter = [0]*(M+1)
+        
+        for item in nums:
+            counter[item] += 1
+                
+        starting_index = 0
+        for i, count in enumerate(counter):
+            counter[i] = starting_index
+            starting_index += count
+                
+        sorted_list = [0]*len(nums)
+        for item in nums:
+            sorted_list[counter[item]] = item
+            counter[item]+=1
+            
         for i in range(len(nums)):
-            min_index = i
-            
-            for j in range(i+1, len(nums)):
-                if nums[min_index] > nums[j]:
-                    min_index = j
-            
-            nums[min_index], nums[i] =   nums[i] , nums[min_index]
+            nums[i] = sorted_list[i]
