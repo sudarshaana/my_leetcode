8a4d77084cb804ab38b08092ad64c7cf229515db
Sudarshan Mondal <sudarshaana@gmail.com>
2022-09-27 21:39:21 +0600

    Time: 243 ms, Memory: 20 MB - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,59 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        # c = Counter(nums)
-        # a = [key for key, _ in c.most_common(k) ]
-        # return a
         
+        # ----- using bucket sort ----
         items = {}
         for num in nums:
             items[num] = items.get(num, 0)+1
         
-        q = sorted(list(items.keys()), key=lambda x: -items[x])
-        return q[:k]
+        buckets = [[] for _ in range(len(nums))]
+        
+        for key, v in items.items():
+            buckets[v-1].append(key)
+        
+        ans = []
+        for bucket in buckets[::-1]:
+            if bucket:
+                ans.extend(bucket)
+        
+        return ans[:k]
+        
+#         heap = data
+#         heapq.heapify(heap)
+#         ans = []
+#         for item in range(k):
+#             x = heapq.heappop(heap)
+#             ans.append(x[1])
+        
+#         return ans
+        
+        
+        
+        
+        # c = Counter(nums)
+        # a = [key for key, _ in c.most_common(k) ]
+        # return a
+        
+#         items = {}
+#         for num in nums:
+#             items[num] = items.get(num, 0)+1
+        
+#         q = sorted(list(items.keys()), key=lambda x: -items[x])
+#         return q[:k]
+
+# using heap
+#         items = {}
+#         for num in nums:
+#             items[num] = items.get(num, 0)+1
+        
+#         data = [(-v, k) for k, v in items.items()]
+        
+#         heap = data
+#         heapq.heapify(heap)
+#         ans = []
+#         for item in range(k):
+#             x = heapq.heappop(heap)
+#             ans.append(x[1])
+        
+#         return ans
