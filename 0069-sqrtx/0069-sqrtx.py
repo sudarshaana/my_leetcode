c05504363595222b14520756967ea46fe0e94704
Sudarshan Mondal <sudarshaana@gmail.com>
2022-10-11 00:18:02 +0600

    Time: 69 ms (53.70%), Space: 13.9 MB (11.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,23 @@
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        #return int(math.sqrt(x))
+        
+        
+        if x < 2:
+            return x
+        
+        left, right = 2, x//2
+        
+        while left <= right:
+            pivot = left + (right-left)//2
+            num = pivot * pivot
+            
+            if num > x:
+                right = pivot - 1
+            elif num < x:
+                left = pivot + 1
+            else:
+                return pivot
+            
+        return right
