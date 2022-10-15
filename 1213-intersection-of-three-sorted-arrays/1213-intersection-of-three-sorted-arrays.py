055bfa24f0ba2b8dc4ebe77db73ded6069f500a6
Sudarshan Mondal <sudarshaana@gmail.com>
2022-10-15 21:34:48 +0600

    Time: 76 ms (99.36%), Space: 14.3 MB (42.03%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
@@ -1,32 +1,39 @@
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
         
-        appears = []
+#         appears = []
         
-        for item in arr1:
-            if self.search(arr2, item):
-                appears.append(item)
+#         for item in arr1:
+#             if self.search(arr2, item):
+#                 appears.append(item)
                 
-        app = []
-        for item in appears:            
-            if self.search(arr3, item):
-                app.append(item)
+#         app = []
+#         for item in appears:            
+#             if self.search(arr3, item):
+#                 app.append(item)
         
         
-        return app
+#         return app
+
+
+            s1 = set(arr1)
+            s2 = set(arr2)
+            s3 = set(arr3)
+            
+            return sorted((s1.intersection(s2)).intersection(s3))
     
-    def search(self, nums, target):
-        l, r = 0, len(nums)-1
+#     def search(self, nums, target):
+#         l, r = 0, len(nums)-1
         
-        while l <= r:
-            mid = l + (r-l)//2
+#         while l <= r:
+#             mid = l + (r-l)//2
             
-            if nums[mid] == target:
-                return True
+#             if nums[mid] == target:
+#                 return True
             
-            elif nums[mid] > target:
-                r = mid - 1
-            else:
-                l = mid + 1
+#             elif nums[mid] > target:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
         
-        return False
+#         return False
