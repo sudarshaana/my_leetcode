f891489e54bc73d64d6312fe25e0012594ec82bc
Sudarshan Mondal <sudarshaana@gmail.com>
2022-10-20 00:48:58 +0600

    Time: 49 ms, Memory: 14.1 MB - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,20 +1,21 @@
 class Solution:
-    def intersection(self, nums, left, right):
-        if nums[right] > nums[left]:
-            return 0
-
-        while left <= right:
-            mid = left + (right-left)//2
-
-            if nums[mid] > nums[mid+1]:
-                 return mid + 1
-            else:
-                if nums[mid] < nums[left]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-                    
     def findMin(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        
+        if nums[0] < nums[-1]:
             return nums[0]
-        return nums[self.intersection(nums, 0, len(nums)-1)]
+        
+        left, right = 0, len(nums)-1
+        
+        while left < right:
+            mid = (left+right)//2
+            
+            if nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+            
+            elif nums[mid] > nums[0]:
+                left = mid+1
+            else:
+                right = mid
+        
+        
+        return nums[left]
