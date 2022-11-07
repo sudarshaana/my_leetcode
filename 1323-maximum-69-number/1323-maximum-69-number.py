fb0f1eaa50f6e64318a7ee67af308881bfdbd215
Sudarshan Mondal <sudarshaana@gmail.com>
2022-11-08 00:06:21 +0600

    Time: 75 ms (5.96%), Space: 13.9 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,13 +1,22 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         
-        new_num = []
-        for i, n in enumerate(str(num)):
-            if n == '6':
-                new_num.append('9')
-                new_num.append(str(num)[i+1:])
-                break
-            else:
-                new_num.append(n)
+#         new_num = []
+#         for i, n in enumerate(str(num)):
+#             if n == '6':
+#                 new_num.append('9')
+#                 new_num.append(str(num)[i+1:])
+#                 break
+#             else:
+#                 new_num.append(n)
         
-        return int("".join(new_num))
+#         return int("".join(new_num))
+
+        nums = list(str(num))
+    
+        for i, cur_num in enumerate(nums):
+            if cur_num == '6':
+                nums[i] = '9'
+                break
+                
+        return int("".join(nums))
