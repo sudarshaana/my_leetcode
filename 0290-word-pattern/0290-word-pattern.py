6685ecc9322f27180dea11fef1b78c4d2630b60f
Sudarshan Mondal <sudarshaana@gmail.com>
2022-11-24 00:08:00 +0600

    Time: 42 ms (76.25%), Space: 13.9 MB (23.89%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,25 +1,26 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         
-        pattern_dict = {}
-        pat = ""
-        for char in pattern:
-            if char in pattern_dict:
-                pat += pattern_dict[char]
-            else:
-                pattern_dict[char] = str(len(pattern_dict)+1)
-                pat += pattern_dict[char]
+#         pattern_dict = {}
+#         pat = ""
+#         for char in pattern:
+#             if char in pattern_dict:
+#                 pat += pattern_dict[char]
+#             else:
+#                 pattern_dict[char] = str(len(pattern_dict)+1)
+#                 pat += pattern_dict[char]
                 
-        print(pat)
         
-        s_dict = {}
-        ss = ""
-        for char in s.split():
-            if char in s_dict:
-                ss += s_dict[char]
-            else:
-                s_dict[char] = str(len(s_dict)+1)
-                ss += s_dict[char]
-        print(ss)
+#         s_dict = {}
+#         ss = ""
+#         for char in s.split():
+#             if char in s_dict:
+#                 ss += s_dict[char]
+#             else:
+#                 s_dict[char] = str(len(s_dict)+1)
+#                 ss += s_dict[char]
         
-        return pat == ss
+#         return pat == ss
+            
+        s, pattern = s.split(), list(pattern)
+        return list(map(s.index, s)) == list(map(pattern.index, pattern))
