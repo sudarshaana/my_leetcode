c9edb1083f09b57be491a5cc34b19f88b8120848
Sudarshan Mondal <sudarshaana@gmail.com>
2023-12-12 12:29:28 +0600

    Time: 46 ms, Memory: 16.2 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
@@ -1,31 +1,28 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
-#         d = {}
-#         max_num =max(nums)
-        
-#         for i, num in enumerate(nums):
-#             if num*2>max_num & num!=max_num:
-#                 return -1
+        max_num =max(nums)        
+        index = nums.index(max_num)
+        nums.remove(max_num)
+        if max_num >= max(nums)*2:
+            return index
 
-#         return nums.find(max_num)
+        return -1
 
-        # another approach: find max & 2nd max, them check if max >= 2*end Max
+#         # another approach: find max & 2nd max, them check if max >= 2*end Max
     
+#         f_max = nums[0]
+#         s_max = None
+#         max_index = 0
         
-    
-        f_max = nums[0]
-        s_max = None
-        max_index = 0
+#         for i, num in enumerate(nums[1:]):
+#             if num > f_max:
+#                 s_max = f_max
+#                 f_max = num
+#                 max_index = i+1
+#             elif not s_max or num > s_max:
+#                 s_max = num
         
-        for i, num in enumerate(nums[1:]):
-            if num > f_max:
-                s_max = f_max
-                f_max = num
-                max_index = i+1
-            elif not s_max or num > s_max:
-                s_max = num
+#         # print(f_max, s_max, max_index)
         
-        # print(f_max, s_max, max_index)
-        
-        return max_index if f_max >= 2*s_max else -1
+#         return max_index if f_max >= 2*s_max else -1
         
