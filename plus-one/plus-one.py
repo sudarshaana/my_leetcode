5fe5d67e77e49ad2091cbb71bc853cd3a79e46dd
Sudarshan Mondal <sudarshaana@gmail.com>
2023-12-12 12:43:18 +0600

    Time: 48 ms, Memory: 16.2 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,9 +1,30 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-        number = "".join(str(i) for i in digits)
+#         number = "".join(str(i) for i in digits)
         
-        number = int(number)+1
-        number_str = str(number)
+#         number = int(number)+1
+#         number_str = str(number)
         
-        return [int(d) for d in number_str]
+#         return [int(d) for d in number_str]
+
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            
+            d = digits[i]
+            res = d+carry
+            # print(i, d, res)
+
+            if res>9:
+                carry = 1
+                digits[i] = int(str(res)[-1])
+                # print(int(str(res)[-1]))
+            else:
+                digits[i] = res
+                carry = 0
+                
+        if carry ==1:
+            digits.insert(0, 1)
+        
+        return digits
+            
