4d2256e2f32ee4deba40c347bff96042186d3666
Sudarshan Mondal <sudarshaana@gmail.com>
2023-12-12 12:46:52 +0600

    Time: 39 ms, Memory: 16.3 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -8,23 +8,38 @@ class Solution:
         
 #         return [int(d) for d in number_str]
 
-        carry = 1
-        for i in range(len(digits)-1, -1, -1):
+#         carry = 1
+#         for i in range(len(digits)-1, -1, -1):
             
-            d = digits[i]
-            res = d+carry
-            # print(i, d, res)
+#             d = digits[i]
+#             res = d+carry
+#             # print(i, d, res)
 
-            if res>9:
-                carry = 1
-                digits[i] = int(str(res)[-1])
-                # print(int(str(res)[-1]))
-            else:
-                digits[i] = res
-                carry = 0
+#             if res>9:
+#                 carry = 1
+#                 digits[i] = int(str(res)[-1])
+#                 # print(int(str(res)[-1]))
+#             else:
+#                 digits[i] = res
+#                 carry = 0
                 
-        if carry ==1:
-            digits.insert(0, 1)
+#         if carry ==1:
+#             digits.insert(0, 1)
         
-        return digits
+#         return digits
             
+            n = len(digits)
+    
+            # Traverse the array from right to left
+            for i in range(n - 1, -1, -1):
+                # If digit is 9, change it to 0
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    # If not 9, increment it by 1 and return the array
+                    digits[i] += 1
+                    return digits
+
+            # Special case when all digits are 9
+            # Insert a 1 at the beginning of the array
+            return [1] + digits
